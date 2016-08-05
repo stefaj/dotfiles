2d1d5dc011e67de14e93107bcb0adda33f629a44
Stefan Jacholke <stefanjacholke@gmail.com>
2016-08-05 11:33:32 +0200

    Arch install script

--- arch.sh
+++ arch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "Awesome Arch Architectural Appointer"
 cd ~
 mkdir ~/tmp44
 cd tmp44
@@ -9,13 +9,45 @@ wget https://aur.archlinux.org/cgit/aur.git/snapshot/packer.tar.gz
 tar -xvf packer.tar.gz
 cd packer
 makepkg -sri
+# Cabal
+echo "Installing cabal"
+sudo pacman -S cabal-install
 # i3 and urxvt and zsh
+echo "Installing a kickass terminal"
 sudo pacman -S i3 feh rxvt-unicode ttf-droid terminus-font compton zsh-syntax-highlighting zsh
 # i3 gaps
+echo "Randomizing"
+echo "Installing some random window manager"
 packer -S i3-gaps-git dmenu2 powerline-fonts-git unclutter-xfixes-git
 # Pathogfen
+echo "Installing some vim stuff"
 packer -S vim-pathogen
 # Vundle
 git clone https://github.com/gmarik/Vundle.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-
+# Vim
+mkdir ~/.vim/bundle
+cd ~/.vim/bundle
+git clone https://github.com/kristijanhusak/vim-hybrid-material
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/godlygeek/tabular.git
+git clone https://github.com/ervandew/supertab.git
+git clone https://github.com/Shougo/neocomplete.vim.git
+git clone https://github.com/Shougo/vimproc.vim.git
+cd vimproc.vim
+make
+# Haskell vim
+cd ~/.vim/bundle
+git clone https://github.com/eagletmt/ghcmod-vim.git
+git clone https://github.com/eagletmt/neco-ghc
+cabal install ghc-mod hlint
+# Delete tmp
+rm -rf ~/tmp44
