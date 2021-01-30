18b4e59b53101db13003f1756f8d96da451b6844
Stefan J <stefanjacholke@gmail.com>
2021-01-30 00:00:37 -0800

    Removed deprecated snipmate

--- arch.sh
+++ arch.sh
@@ -35,7 +35,6 @@ git clone https://github.com/ctrlpvim/ctrlp.vim.git
 git clone https://github.com/scrooloose/syntastic.git
 git clone https://github.com/tomtom/tlib_vim.git
 git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
-git clone https://github.com/garbas/vim-snipmate.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/scrooloose/nerdcommenter.git
 git clone https://github.com/godlygeek/tabular.git
